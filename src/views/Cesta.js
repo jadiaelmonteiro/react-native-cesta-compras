eae01c7e946695d561268107936b0b64e4f5f05a
Jadiael Monteiro <jadiael.monteiro@gmail.com>
2022-06-15 13:27:23 -0300

    Title

--- src/views/Cesta.js
+++ src/views/Cesta.js
@@ -1,8 +1,32 @@
 import React from "react";
-import { Text } from "react-native";
+import { Dimensions, Image, StyleSheet, Text } from "react-native";
+
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
     return (
-        <Text>Página Cesta</Text>
+        <>
+            <Image source={topo} style={styles.topo} />
+            <Text style={styles.title}> Detalhes da cesta</Text>
+        </>
     );
 }
+
+const styles = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    title: {
+        width: "100%",
+        position: "absolute", //texto absoluto a imagem
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
+    }
+});
